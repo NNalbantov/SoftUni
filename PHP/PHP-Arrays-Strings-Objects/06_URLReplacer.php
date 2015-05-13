3ff3508ed3717d3f5e61a7d32d8e237054512ea2
NNalbantov <steele@abv.bg>
2015-05-13 18:08:07 +0300

    Create 06_URLReplacer.php

---
+++ PHP/PHP-Arrays-Strings-Objects/06_URLReplacer.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>URL Replacer</title>
+</head>
+<body>
+    <form action="" method="get">
+        <textarea name="text"></textarea>
+        <input type="submit" name="Submit"/>
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_GET['text'])){
+        $text = $_GET['text'];
+        $result = preg_replace("/(<a\s+?href=)/", "[URL=", $text);
+        $result = preg_replace("/\\\"(\>)/", "] ", $result);
+        $result = preg_replace("/\<\/a\>/", "[/URL]", $result);
+        echo htmlentities($result);
+    }
+?>
