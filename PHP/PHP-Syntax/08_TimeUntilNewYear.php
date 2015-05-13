72518af27121e8f5178705f9194ed7af59ca87d4
NNalbantov <steele@abv.bg>
2015-05-13 17:54:59 +0300

    Update 08_TimeUntilNewYear.php

--- PHP/PHP-Syntax/08_TimeUntilNewYear.php
+++ PHP/PHP-Syntax/08_TimeUntilNewYear.php
@@ -1,6 +1,5 @@
 <?php
 date_default_timezone_set ("Europe/Sofia");
-
 $currentTime = getdate();
 $unixTime = $currentTime[0];
 $unixNewYearTime = strtotime("01 january 2016");
@@ -15,7 +14,6 @@ $remainingHours = floor($remainingSeconds / 3600);
 $remainingSeconds = $remainingSeconds - $remainingHours * 3600;
 $remainingMinutes = floor($remainingSeconds / 60);
 $remainingSeconds = $remainingSeconds - $remainingMinutes * 60;
-
 
 echo 'Hours until new year : ' . $hours . "<br/>";
 echo 'Minutes until new year : ' . $minutes . "<br/>";
