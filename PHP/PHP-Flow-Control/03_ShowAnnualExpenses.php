2146a4b1f6bf35a9c97f0b8c4faaa7aec8661c7f
NNalbantov <steele@abv.bg>
2015-05-13 18:05:28 +0300

    Create 03_ShowAnnualExpenses.php

---
+++ PHP/PHP-Flow-Control/03_ShowAnnualExpenses.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show Annual Expenses</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="years">Enter number of years</label>
+        <input type="text" name="years" id="years"/>
+        <input type="submit" value="Show costs"/>
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_POST['years'])){
+        $numberOfYears = $_POST['years'];
+        $currentYear = date("Y");
+?>
+    <?php
+        $sum = 0;
+        echo "<table border='1'>";
+        echo "<tr style='font-weight: bold' >
+                <td>Year</td>
+                <td>January</td>
+                <td>February</td>
+                <td>March</td>
+                <td>April</td>
+                <td>May</td>
+                <td>June</td>
+                <td>July</td>
+                <td>August</td>
+                <td>September</td>
+                <td>October</td>
+                <td>November</td>
+                <td>December</td>
+                <td>Total:</td>
+            </tr>";
+        for($i = $currentYear; $i > $currentYear - $numberOfYears; $i--){
+            echo "<tr>";
+            echo "<td>$i</td>";
+            for($j = 0; $j < 12; $j++){
+                $monthExp = mt_rand(0,999);
+                $sum += $monthExp;
+                echo "<td>$monthExp</td>";
+            }
+            echo "<td>$sum</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        ?>
+    <?php } ?>
